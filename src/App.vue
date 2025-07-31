b06b05b5ae5e7cb3d96d7559818b1b206e04addd
Andrei Daneliuc <daneliucilieandrei@gmail.com>
2025-07-31 20:16:47 +0300

    Create project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Button from './components/Button.vue'
+import { ref } from 'vue'
+
+const loading = ref(false)
+</script>
+
+<template>
+  <div class="flex h-screen w-screen items-center justify-center">
+    <Button :loading="loading" @click="loading = true">
+      asdasfasdfsadsafasdasfdasdfassafdsadsadsadsadasdsa
+    </Button>
+  </div>
+</template>
---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { vAutoAnimate } from '@formkit/auto-animate/vue'
+
+defineProps<{
+  loading?: boolean
+  disabled?: boolean
+}>()
+</script>
+
+<template>
+  <button
+    ref="button"
+    class="flex cursor-pointer items-center justify-center gap-2 rounded bg-amber-500 px-4 py-2 text-white disabled:cursor-not-allowed disabled:opacity-50"
+    :disabled="disabled || loading"
+  >
+    <div v-auto-animate>
+      <div
+        v-if="loading"
+        class="h-4 w-4 animate-spin rounded-full border-2 border-white border-t-transparent"
+      />
+    </div>
+    <slot />
+  </button>
+</template>
